d088175d653388d548b000607d2a36b1ee99f173
Artem-Borzov <abug990@gmail.com>
2023-03-07 00:05:50 +0200

    local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,12 +7,7 @@ import Filter from 'components/Filter/Filter';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -48,6 +43,21 @@ export default class App extends Component {
       contacts: prevState.contacts.filter(({ id }) => id !== delId),
     }));
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('componentDidUpdate');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
